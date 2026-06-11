523aa9034b03b3acec3663125d21c25111d50731
Glory-to-God <forest4994@handong.ac.kr>
2026-06-11 12:33:26 +0900

    Add Analysis

--- 0603_Good/MATLAB/Stabilization/Stabilization_Mod.m
+++ 0603_Good/MATLAB/Stabilization/Stabilization_Mod.m
@@ -5,9 +5,10 @@ clc;
 clear all;
 close all;
 
-Ts      = 0.005;    %[sec]
-Tf      = 2;        %[sec]
-time    = 0:Ts:Tf;
+Tstep   = 0.001;    %[sec] for looking 0.005 [sec] sampling effect
+Tsample = 0.005;    %[sec] 
+Tf      = 10;        %[sec]
+time    = 0:Tstep:Tf;
 
 s       = tf('s');
 
@@ -22,42 +23,48 @@ Gm          = Km / (Time_const*s + 1); % Motor bandwidth 2.7723 [Hz]
 %==========================================================================
 % 1. Closed loop Values -> 3rd order system
 Zeta_cl     = 1/sqrt(2);    % [-]
-figure(1); hold on;
-for iter = 1:1:10
-    W_cl        = 18 + iter;         % [rad/s]
-    
-    % 2. tf of Gc
-    Kp          = (2*W_cl*Zeta_cl*Time_const - 1)/Km; % [-]
-    Ki          = (Time_const/Km)*W_cl^2;             % [V/rad] 
-    Gc          = Kp+Ki/s;
-    
-    % 3. System tf
-    Go              = Gc*Gm;
-    Gcl             = Go/(1+Go);
-    
-    % 4. Margin
-    [GM, PM, ~, ~]  = margin(Go);
-    GM_dB = 20 * log10(GM);
-
-    % 5. Pzmap
-    pz_label = sprintf('W_{cl}:%.2f[rad/s], GM: %.2f dB, PM: %.2f^o', W_cl, GM_dB, PM);
-    pz_Gcl = minreal(Gcl);
-    pz_Gcl.Name = pz_label;
-    pzmap(pz_Gcl);
-    legend;
-
-end
-hold off;
+% figure(1); hold on;
+% for iter = 1:1:10
+%     W_cl        = 18 + iter;         % [rad/s]
+% 
+%     % 2. tf of Gc
+%     Kp          = (2*W_cl*Zeta_cl*Time_const - 1)/Km; % [-]
+%     Ki          = (Time_const/Km)*W_cl^2;             % [V/rad] 
+%     Gc          = Kp+Ki/s;
+% 
+%     % 3. System tf
+%     Go              = Gc*Gm;
+%     Gcl             = Go/(1+Go);
+% 
+%     % 4. Margin
+%     [GM, PM, ~, ~]  = margin(Go);
+%     GM_dB = 20 * log10(GM);
+% 
+%     % 5. Pzmap
+%     pz_label = sprintf('W_{cl}:%.2f[rad/s], GM: %.2f dB, PM: %.2f^o', W_cl, GM_dB, PM);
+%     pz_Gcl = minreal(Gcl);
+%     pz_Gcl.Name = pz_label;
+%     pzmap(pz_Gcl);
+%     legend; grid on;
+% 
+% end
+% hold off;
 
 %==========================================================================
 % Simulink Values
 %==========================================================================
-W_cl        = 23;         % [rad/s]
+W_cl        = 26;         % [rad/s]
     
 % 2. tf of Gc
 Kp          = (2*W_cl*Zeta_cl*Time_const - 1)/Km; % [-]
 Ki          = (Time_const/Km)*W_cl^2;             % [V/rad] 
 
 % 3. Other Values
-Sim_disturbance_amplitude   = 8 ; % [rad/s]
-Sim_disturbance_freq        = pi; % [rad/s] = 0.5[Hz]
+Sim_disturbance_amplitude   = 8 ;  % [rad/s]
+Sim_disturbance_freq        = pi;  % [rad/s] = 0.5[Hz]
+
+Sim_deadZone_start          = -0.5; % [rad/s]
+Sim_deadZone_final          = 0.5;  % [rad/s]
+
+Sim_saturation_neg          = -20;   % [rad/s]
+Sim_saturation_pos          = 20;   % [rad/s]
